7bdadf0ba66179b400f9e4236328023afe842d49
nbeymer99 <nbeymer99@gmail.com>
2025-08-26 23:40:59 -0400

    Add project files.

---
+++ WeddingReminder/ModEntry.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using StardewValley.Locations;
+
+namespace WeddingReminder
+{
+    /// <summary>The mod entry point.</summary>
+    internal sealed class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        public override void Entry(IModHelper helper)
+        {
+            // Run this behaviour every day, before saving
+            helper.Events.GameLoop.DayStarted += this.GameLoop_DayStarted;
+            helper.Events.Player.Warped += this.GameLoop_PlayerWarpFarmhouse;
+        }
+
+        private void GameLoop_DayStarted(object sender, DayStartedEventArgs e)
+        {
+            if (Game1.player.GetSpouseFriendship() is Friendship spouse && spouse.CountdownToWedding == 1)
+            {
+                Game1.addHUDMessage(new HUDMessage(this.Helper.Translation.Get("WeddingReminder.HUDMessage")));
+            }
+        }
+
+        private void GameLoop_PlayerWarpFarmhouse(object sender, WarpedEventArgs e)
+        {
+            if (Game1.player.GetSpouseFriendship() is Friendship spouse && spouse.CountdownToWedding == 1 && e.NewLocation is FarmHouse house && house.owner == Game1.player)
+            {
+                Game1.addHUDMessage(new HUDMessage(this.Helper.Translation.Get("WeddingReminder.HUDMessage")));
+            }
+        }
+    } 
+}
